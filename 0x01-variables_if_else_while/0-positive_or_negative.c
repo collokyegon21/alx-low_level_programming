5aa4dce7bbf6b1556a784bb8df1559d3f207c8aa
collokyegon21 <collo.kyegon@hmail.com>
2022-09-09 10:20:19 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,10 @@
  *                                                                                                                             
  *Return: Always 0                                                                                                               */  
 {
-	int n ;
+int n ;
+
+
+
 	srand(time(0));
 	if (n > 0)
 	{
