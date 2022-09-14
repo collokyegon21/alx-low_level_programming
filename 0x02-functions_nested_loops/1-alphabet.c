dcf6ed15232778de73f24bca71e8d552f1c1b79f
collokyegon21 <collo.kyegon@hmail.com>
2022-09-13 23:56:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lowercase 
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
