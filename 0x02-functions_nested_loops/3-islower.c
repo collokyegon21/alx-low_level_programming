8ab4aa53bbd628ec678bb5522f0af00b7e59d08e
collokyegon21 <collo.kyegon@hmail.com>
2022-09-14 00:52:52 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,12 @@
 /**
  * _islower - checks function for lower case
  *
+ * @c: the alphabet to be checked in the function
+ *
  * Return: Always 0.
  *
  */
+
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
