73862a69749ba06f361e750e3c2a8e55591eb1b4
collokyegon21 <collo.kyegon@hmail.com>
2022-09-17 00:11:10 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - this function prints numbers 0 - 9
+ *
+ * @i: variable i
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
