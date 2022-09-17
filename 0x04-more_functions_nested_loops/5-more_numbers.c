90c7e66b77d1dab745a2a7f4dc463525f2aa7590
collokyegon21 <collo.kyegon@hmail.com>
2022-09-17 00:40:35 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 - 14 10 times
+ *
+ * @i: i variable
+ * @x: x variable
+ *
+ * Return: always 0
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			if (x > 9)
+			{
+				_putchar(x / 10 + '0');
+			}
+			_putchar(x % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
