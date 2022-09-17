5167c2f62996c9e23ce77b09f679ad99f082bfcd
collokyegon21 <collo.kyegon@hmail.com>
2022-09-17 00:46:26 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line on terminal
+ *
+ * @x: variable x
+ * @n: variable n
+ *
+ * Return : 0 always
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	for (x = 1; x <= n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
