3f092efae6194d734e5c9e2cdc6296c8991fb971
collokyegon21 <collo.kyegon@hmail.com>
2022-09-17 00:05:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - function to multiply to int variables
+ * @a: variable a
+ * @a: variable b
+ *
+ * Return:0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
