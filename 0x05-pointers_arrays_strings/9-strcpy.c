78f59a28f0bc5dff9adc24076c73a704f4423207
collokyegon21 <collo.kyegon@hmail.com>
2022-09-20 04:29:25 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
-	do
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
 		dest[i] = src[i];
-	}while(src[i] != '\0');
-
+	}
+	dest[i] = '\0';
 	return (dest);
 }
