964b4280edda1be0d22c822e59b514abb4cf07c0
collokyegon21 <collo.kyegon@gmail.com>
2022-10-01 01:22:19 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
-while (i < n)
-{
-	*(dest + i) = *(src + i);
-	i++;
-}
-return (dest);
+	for (i = 0; n > 0 ; i++, n--)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
