f8d084696347e9cc28713a2588e7cba332d6c4dc
collokyegon21 <collo.kyegon@gmail.com>
2022-10-02 09:58:01 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,11 +11,7 @@ int _strlen_recursion(char *s)
 	int len;
 
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
+	len = _strlen_recursion(s + 1);
+	return (len + 1);
 }
