c3cb0ef6415ca4cdb201a3978bc529aacf1a42dd
collokyegon21 <collo.kyegon@gmail.com>
2022-10-02 10:10:04 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* factorial - returns factorial of a given number
+* @n: number to factorize
+* Return: int
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
