0e42bd4ce6660c8f7e05be9ebfeed91f6329679a
collokyegon21 <collo.kyegon@gmail.com>
2022-10-03 04:58:27 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints its name followed by new line
+* @argc: count
+* @argv:list
+* Return: 0
+*/
+
+int main(int argc, char argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0];
+	return (0);
+}
