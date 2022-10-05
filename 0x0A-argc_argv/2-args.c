3541801fde3dd1a30b688586b3c202acf1d934e7
collokyegon21 <collo.kyegon@gmail.com>
2022-10-05 02:19:08 -0700

    readme.md

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,16 +7,13 @@
 * Return: 0 or 1
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc > 0)
+	for (i = 0; i > argc; i++)
 	{
-		for (i = 0; i > argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
