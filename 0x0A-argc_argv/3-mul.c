e19a4935e9a8ce3ff0a1e71ec034ed9a890d2e67
collokyegon21 <collo.kyegon@gmail.com>
2022-10-05 22:55:19 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiples two numbers
+* @argc: count
+* @argv: pointer to number of array
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	int a;
+	int b;
+
+	if (agrc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2];
+		 printf("%d\n", a * b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
