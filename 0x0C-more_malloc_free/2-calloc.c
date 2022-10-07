f1a6de3e5f564c4e5f771767e909c2653246f357
collokyegon21 <collo.kyegon@gmail.com>
2022-10-07 00:55:01 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	int *p;
 
-	if (nemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	/*p = (int*)calloc(j, sizeof(int))*/
