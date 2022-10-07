bbcb9af427dc86347c7d40e6812c46234883b081
collokyegon21 <collo.kyegon@gmail.com>
2022-10-07 12:44:36 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* numberlen - calculates number of words
+* @str: string being passed to check length
+*
+* Return: number of words
+*/
+
+int numberlen(char *str)
+{
+	int a, b = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			b++;
+		}
+	}
+	return (b);
+}
+
+
+/**
+* **strtow - splits a string into words
+* @str: string to print
+* Return: pointer to an array of string
+*/
+
+char **strtow(char *str)
+{
+	char **p, *ptr;
+	int words = 0, b = 0, c = 0, len = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	words = numberlen(str);
+	if (words == 0)
+		return (NULL);
+	p = malloc((words + 1) * sizeof(char *));
+	if (p == 0)
+		return (NULL);
+	for (; *str != '\0' &&  b < words;)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			ptr = str;
+			for (; *str != ' ' && *str != '\0';)
+			{
+					len++;
+					str++;
+			}
+			p[b] = malloc((len + 1) * sizeof(char));
+			if (p[b] == 0)
+			{
+				free(p, b);
+				return (NULL);
+			}
+			while (*ptr != ' ' && *ptr != '\0')
+			{
+				p[b][c] = *ptr;
+				ptr++;
+				c++;
+			}
+			p[b][c] = '\0';
+			b++; c = 0; len = 0; str++;
+		}
+	}
+	return (p);
+}
