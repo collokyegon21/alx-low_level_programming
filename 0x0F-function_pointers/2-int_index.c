8867a01a49e8067e7cdcf0847ea9a0f5a564be0a
collokyegon21 <collo.kyegon@gmail.com>
2022-10-14 03:12:15 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+* int_index - function that searches fot an interger
+* @array: the array for an interger
+* @size: the number of the elements in the array
+* @cmp:pointer to the comparison function
+*
+* Return:  the index of the first element for which the cmp function does not return 0
+* If no element matches, return -1 or  if size <= 0
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || !array || !cmp)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}	
