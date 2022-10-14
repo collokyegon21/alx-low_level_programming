ff5808ab5da934df0fbbed245f5cec001d489152
collokyegon21 <collo.kyegon@gmail.com>
2022-10-14 01:43:56 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointer.h"
+
+/**
+* print_name - prints name of a given parameter
+* @name:the name to print
+* @f: function to print the name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		exit(98);
+	(*f)(name);
+}
