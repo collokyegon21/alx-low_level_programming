4bef86c188840ca64222b3c8b7fd78f21999837f
collokyegon21 <collo.kyegon@gmail.com>
2022-10-15 01:26:05 -0700

    prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* variadic_functions_h */
